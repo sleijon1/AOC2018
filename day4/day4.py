5ea3eace07f6f566ab5812849cfb12a68e7abaee
sleijon1 <simonleijon59@gmail.com>
2020-09-20 17:17:28 +0200

    part 2 and comments

--- day4/day4.py
+++ day4/day4.py
@@ -8,6 +8,10 @@ import datetime
 class Guard:
     """ A class representing a guard"""
     def __init__(self, id_num):
+        """ constructor Guard
+        Keyword args:
+        id_num = the id of the guard
+        """
         self.id_num = id_num
         self.sleep_schedule = {}
 
@@ -16,11 +20,18 @@ class Guard:
         return sum(self.sleep_schedule.values())
 
     def minute_most_sleep(self):
-        """ Returns the minute that the guard sleeps on the most"""
-        max_slept = max(self.sleep_schedule.values())
+        """ Returns the minute that the guard sleeps on the most or none if
+        they havent slept. """
+
+        try:
+            max_slept = max(self.sleep_schedule.values())
+        except ValueError:
+            print("ValueError; Guard: " + str(self.id_num) + ", hasnt slept.")
+            return None, None
+
         for key in self.sleep_schedule.keys():
             if self.sleep_schedule[key] == max_slept:
-                return key
+                return key, max_slept
 
     def add_minute(self, minute):
         """ adds a time slept at minute minute"""
@@ -31,6 +42,9 @@ class Guard:
             # first time slept at this minute
             self.sleep_schedule[minute] = 1
 
+    def __str__(self):
+        return 'Guard: ' + str(self.id_num)
+
     def __eq__(self, other):
         return (
             self.__class__ == other.__class__ and
@@ -38,6 +52,14 @@ class Guard:
         )
 
 def calculate_guard_sleep(guard_schedule):
+    """ generates guards and adds their number of sleep to their
+    sleep schedule
+
+    Keyword args:
+    guard_schedule - the ordered list of guard shifts
+    Returns:
+    list of guards
+    """
     guard_list = []
     current_guard = None
 
@@ -72,18 +94,38 @@ def calculate_guard_sleep(guard_schedule):
 
     return guard_list
 
-def sleepy_guard(guard_list):
+def sleepiest_guard(guard_list):
+    """ calculates which guard slept the most
+    Keyword args:
+    guard_list - list of guards to compare
+    """
     sleepiest_guard = guard_list[0]
     for guard in guard_list:
         if guard.total_sleep() > sleepiest_guard.total_sleep():
             sleepiest_guard = guard
+    return sleepiest_guard
 
-    result = sleepiest_guard.minute_most_sleep() * int(sleepiest_guard.id_num)
-    print(result)
-    return result
+def consistent_guard(guard_list):
+    """ returns the guard that is most consistenly asleep at
+    a specific minute.
+
+    Keyword args:
+    guard_list - list of guards
+    """
+    sleepiest_guard = guard_list[0]
+    for guard in guard_list:
+        minute, max_slept = guard.minute_most_sleep()
+        _, current_max = sleepiest_guard.minute_most_sleep()
+        if(max_slept is None or current_max is None):
+            continue
+        if max_slept > current_max:
+            sleepiest_guard = guard
+
+    return sleepiest_guard
+
 
 def format_and_order_schedule():
-    """formats and order schedule by date
+    """formats and order schedule by date by reading input.txt file
     """
     inp = read_and_strip()
     format_input = list()
@@ -98,5 +140,15 @@ def format_and_order_schedule():
 if __name__ == "__main__":
     format_input = format_and_order_schedule()
     guard_list = calculate_guard_sleep(format_input)
-    sleepy_guard(guard_list)
 
+    sleepiest_guard = sleepiest_guard(guard_list)
+    minute, _ = sleepiest_guard.minute_most_sleep()
+    print("Part 1: " + str(sleepiest_guard) + "\n(" + str(minute) \
+          + " * " + str(sleepiest_guard.id_num) + ") = " + \
+          str(minute * int(sleepiest_guard.id_num)) + "\n")
+
+    consistent_guard = consistent_guard(guard_list)
+    minute, _ = consistent_guard.minute_most_sleep()
+    print("\nPart 2: " + str(consistent_guard) + "\n(" + str(minute) \
+          + " * " + str(consistent_guard.id_num) + ") = " + \
+          str(minute * int(consistent_guard.id_num)))
