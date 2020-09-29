f00d82c0641e5433db648b54278a323f4f252f88
sleijon1 <simonleijon59@gmail.com>
2020-09-29 19:18:05 +0200

    added some things

--- day9/day9.py
+++ day9/day9.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append("../")
 from day1.day1part2 import read_and_strip
 
@@ -11,7 +12,7 @@ def marble_game(players, points):
     # populate player dict
     players_dict = {}
     for player in range(players):
-        players_dict[player] = []
+        players_dict[player] = 0
 
     current_marble = 0
     circle = []
@@ -20,24 +21,26 @@ def marble_game(players, points):
     for marble in range(1, points+1):
         # each round each player takes turn
         player = players_dict[player_index]
+        circle_length = len(circle)
+        current_index = circle.index(current_marble)
         if marble != 0 and marble % 23 == 0:
-            player.append(marble)
-            index_removal = circle.index(current_marble)-7
+            players_dict[player_index] += marble
+            index_removal = current_index-7
             if index_removal < 0:
-                index_removal = len(circle)+index_removal
+                index_removal = circle_length+index_removal
 
             marble_index = index_removal
             # marble 7 steps to the left
-            player.append(circle[marble_index])
-            if marble_index+1 == len(circle):
+            players_dict[player_index] += circle[marble_index]
+            if marble_index+1 == circle_length:
                 current_marble = circle[0]
             else:
                 current_marble = circle[marble_index+1]
             circle.pop(marble_index)
         else:
-            insert_index = circle.index(current_marble)+2
-            if insert_index > len(circle):
-                insert_index = insert_index-len(circle)
+            insert_index = current_index+2
+            if insert_index > circle_length:
+                insert_index = insert_index-circle_length
             circle.insert(insert_index, marble)
             current_marble = marble
 
@@ -59,8 +62,11 @@ def format_input(inp):
 if __name__ == "__main__":
     inp = read_and_strip()
     players, points = format_input(inp)
-    circle, players = marble_game(players, points*100)
-    player_scores = [sum(players[player]) for player in players]
+    start = time.time()
+    circle, players = marble_game(players, points*3)
+    end = time.time()
+    print(end-start)
+    player_scores = [players[player] for player in players]
     print(player_scores)
     print(max(player_scores))
     #print("circle: " + str(circle))
