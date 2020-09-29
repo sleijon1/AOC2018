b019a24b46f56ee3b9609f785bece850e52c9af9
sleijon1 <simonleijon59@gmail.com>
2020-09-29 23:12:07 +0200

    logn insertion solved part2

--- day9/day9.py
+++ day9/day9.py
@@ -2,6 +2,7 @@ import sys
 import time
 sys.path.append("../")
 from day1.day1part2 import read_and_strip
+from blist import *
 
 def marble_game(players, points):
     """ Plays a game of Elfish marble
@@ -15,17 +16,17 @@ def marble_game(players, points):
         players_dict[player] = 0
 
     current_marble = 0
-    circle = []
+    circle = blist([])
     circle.append(current_marble)
     player_index = 0
+    index = 0
     for marble in range(1, points+1):
         # each round each player takes turn
         player = players_dict[player_index]
         circle_length = len(circle)
-        current_index = circle.index(current_marble)
         if marble != 0 and marble % 23 == 0:
             players_dict[player_index] += marble
-            index_removal = current_index-7
+            index_removal = index-7
             if index_removal < 0:
                 index_removal = circle_length+index_removal
 
@@ -34,15 +35,18 @@ def marble_game(players, points):
             players_dict[player_index] += circle[marble_index]
             if marble_index+1 == circle_length:
                 current_marble = circle[0]
+                index = 0
             else:
                 current_marble = circle[marble_index+1]
             circle.pop(marble_index)
+            index = marble_index
         else:
-            insert_index = current_index+2
+            insert_index = index+2
             if insert_index > circle_length:
                 insert_index = insert_index-circle_length
             circle.insert(insert_index, marble)
             current_marble = marble
+            index = insert_index
 
         # cycle players
         if player_index < players-1:
@@ -63,7 +67,7 @@ if __name__ == "__main__":
     inp = read_and_strip()
     players, points = format_input(inp)
     start = time.time()
-    circle, players = marble_game(players, points*3)
+    circle, players = marble_game(players, points*100)
     end = time.time()
     print(end-start)
     player_scores = [players[player] for player in players]
