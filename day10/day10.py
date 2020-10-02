e7dbda861154d493aec6e9b1d356169af407bf9d
sleijon1 <simonleijon59@gmail.com>
2020-10-02 13:43:57 +0200

    renamed things, removed not used code, added comments

--- day10/day10.py
+++ day10/day10.py
@@ -2,39 +2,41 @@ import time
 import sys
 sys.path.append("../")
 from day1.day1part2 import read_and_strip
-from operator import add 
-import numpy as np
+from operator import add
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-
-fig = plt.figure()
-#creating a subplot 
-ax1 = fig.add_subplot(1,1,1)
-#ax1.margins(0)
-
-class LightPoints:
-    """ class representing Santa's light guiding system """
+class DirectionalPoints:
+    """ class representing x, y position and dx, dy velocity """
     def __init__(self, points):
         self.points = points
         self.elapsed = 0
 
     def tick(self):
+        """ moves positions one step with their respective velocities """
         self.elapsed += 1
         for point in self.points:
             point["pos"] = tuple(map(add, point["pos"], point["vel"]))
         return self.elapsed
 
     def positions(self):
+        """ returns all points positions """
         return [point["pos"] for point in self.points]
 
     def read(self):
         return self.points
 
 def mh_distance(p1, p2):
+    """ simple function for computing manhattan distance """
     return (abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]))
 
-def align_stars(points):
+def align_points(points, distance=2):
+    """ Elapses LightStars object points until they are aligned
+    with a distance of distance
+    Keyword args:
+    points -- LightStars object to elapse
+    distance -- Largest distance every point needs to have
+    to at least one other point
+    """
     while(True):
         all_aligned = True
         #print(points.positions())
@@ -42,15 +44,12 @@ def align_stars(points):
         for point1 in points.positions():
             aligned_w_other = False
             for point2 in points.positions():
-                if mh_distance(point1, point2) <= 2 and mh_distance(point1, point2) != 0 :
+                if mh_distance(point1, point2) <= distance and mh_distance(point1, point2) != 0 :
                     aligned_w_other = True
                     i += 1
-                    # found one for which p1 aligns with
                     break
             if not aligned_w_other:
                 all_aligned = False
-                # some point have no alignment
-                # we havent reached alignment
                 break
         if not all_aligned:
             points.tick()
@@ -58,8 +57,15 @@ def align_stars(points):
             break
     return points
 
-def plot_graph(points):
-
+def plot_graph(points, show=False):
+    """ Plots the current positions of the points
+    Keyword args:
+    points -- LightStars object of the stars to plot
+    show -- True if plot graph
+    """
+    fig = plt.figure()
+    # creating a subplot
+    ax1 = fig.add_subplot(1,1,1)
     xs = []
     ys = []
     lines = points.positions()
@@ -71,22 +77,24 @@ def plot_graph(points):
         xs.append(float(x))
         ys.append(float(y))
 
-    ax1.clear()
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.title('Live graph with matplotlib')
 
     plt.plot(xs, ys, linestyle='',marker='o')
-    plt.show()
+    if show:
+        plt.show()
 
-def format_input(points, scale=1, shift=0):
+def format_input(points):
+    """ Formats point information to dict containing
+    position and direction velocity """
     formatted_points = []
     for point in points:
-        pos_x = scale*int(point.split("<")[1].split(">")[0].split(',')[0])+shift
-        pos_y = scale*int(point.split("<")[1].split(">")[0].split(',')[1])+shift
+        pos_x = int(point.split("<")[1].split(">")[0].split(',')[0])
+        pos_y = int(point.split("<")[1].split(">")[0].split(',')[1])
         position = (pos_x, pos_y)
-        vel_x = int(point.split("<")[2].rstrip(">").split(",")[0])+shift
-        vel_y = int(point.split("<")[2].rstrip(">").split(",")[1])+shift
+        vel_x = int(point.split("<")[2].rstrip(">").split(",")[0])
+        vel_y = int(point.split("<")[2].rstrip(">").split(",")[1])
         velocity = (vel_x, vel_y)
         formatted_point = {}
         formatted_point["pos"] = position
@@ -99,12 +107,7 @@ if __name__ == "__main__":
     #inp = read_and_strip(file_name="small_input.txt")
     inp = read_and_strip(file_name="problem_input.txt")
     points = format_input(inp)
-    print(points)
-    lightpoints = LightPoints(points)
-    aligned_points = align_stars(lightpoints)
+    lightpoints = DirectionalPoints(points)
+    aligned_points = align_points(lightpoints)
     print("Elapsed time: " + str(aligned_points.elapsed))
-    plot_graph(aligned_points)
-
-
-    #plt.show()
-
+    plot_graph(aligned_points, show=True)
