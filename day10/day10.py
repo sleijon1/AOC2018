35d5963083f76529769ebe13d98085880c9acd9b
sleijon1 <simonleijon59@gmail.com>
2020-10-02 13:28:51 +0200

    part 2

--- day10/day10.py
+++ day10/day10.py
@@ -102,6 +102,7 @@ if __name__ == "__main__":
     print(points)
     lightpoints = LightPoints(points)
     aligned_points = align_stars(lightpoints)
+    print("Elapsed time: " + str(aligned_points.elapsed))
     plot_graph(aligned_points)
 
 
