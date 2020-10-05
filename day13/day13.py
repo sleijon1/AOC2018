be6e024f5b8a9bc7e28c99ef60f6501db74e61f6
sleijon1 <simonleijon59@gmail.com>
2020-10-05 20:05:01 +0200

    globals for directions, code cleanup, print cleanup

--- day13/day13.py
+++ day13/day13.py
@@ -3,6 +3,10 @@ import sys
 sys.path.append("../")
 from day1.day1part2 import read_and_strip
 
+UP_DOWN = ("^", "v")
+LEFT_RIGHT = ("<", ">")
+UDLR = ("^", "v", "<", ">")
+
 class Cart:
     """ Cart class adhering to specific movement rules """
     def __init__(self, x, y, direction):
@@ -92,14 +96,13 @@ def tick(inp, carts):
     updated = []
     for y, row in enumerate(inp):
         for x, col in enumerate(row):
-            if col == '<' or col == '>' \
-               or col == '^' or col == 'v':
+            if col in UDLR:
                 check_cart = Cart(x, y, col)
                 if (x, y) in updated:
                     continue
                 if check_cart in carts:
                     cart = carts[carts.index(check_cart)]
-                    cart.move() 
+                    cart.move()
                     new_x = cart.x
                     new_y = cart.y
                     old_pos = cart.position
@@ -116,12 +119,12 @@ def tick(inp, carts):
     return None
 
 def tick_two(inp, carts):
-    """ moves each cart once, returns position on collison otherwise None """
+    """ moves each cart once, returns position of car when single car is left
+    otherwise None """
     updated = []
     for y, row in enumerate(inp):
         for x, col in enumerate(row):
-            if col == '<' or col == '>' \
-               or col == '^' or col == 'v':
+            if col in UDLR:
                 check_cart = Cart(x, y, col)
                 if (x, y) in updated:
                     continue
@@ -134,9 +137,8 @@ def tick_two(inp, carts):
                     inp[y][x] = old_pos
                     collisions = [c for c in carts if c.x == new_x and c.y == new_y and c is not cart]
                     if len(collisions) > 0:
-                        print(collisions)
                         inp[new_y][new_x] = collisions[0].position # elfs clean up scraps
-                        print("Carts collided! They collided at: " + str((new_x, new_y)))
+                        #print("Carts collided! They collided at: " + str((new_x, new_y)))
                         carts.remove(cart)
                         carts.remove(collisions[0])
                     else:
@@ -145,8 +147,8 @@ def tick_two(inp, carts):
                         updated.append((new_x, new_y))
                         
     if len(carts) == 1: # last survivor
-        print("One cart left: " + str((carts[0].x, carts[0].y)))
-        print(carts)
+        #print("One cart left: " + str((carts[0].x, carts[0].y)))
+        #print(carts)
         return (carts[0].x, carts[0].y)
     else:
         return None
@@ -158,18 +160,18 @@ def create_carts(inp):
     for y, row in enumerate(inp):
         for x, col in enumerate(row):
             cart = None
-            if col == '<' or col == '>':
+            if col in LEFT_RIGHT:
                 cart = Cart(x, y, col)
                 cart.position = '-'
                 carts.append(cart)
-            elif col == '^' or col == 'v':
+            elif col in UP_DOWN:
                 cart = Cart(x, y, col)
                 cart.position = '|'
                 carts.append(cart)
     return carts
 
 def tick_til_one_left(inp):
-    """ ticks all carts until collision happens"""
+    """ ticks all carts until only one car left """
     for i in range(len(inp)):
         inp[i] = list(inp[i])
     carts = create_carts(inp)
@@ -181,7 +183,7 @@ def tick_til_one_left(inp):
     return result
 
 def tick_til_crash(inp):
-    """ ticks all carts until collision happens"""
+    """ ticks all carts until collision happens """
     for i in range(len(inp)):
         inp[i] = list(inp[i])
     carts = create_carts(inp)
@@ -198,13 +200,19 @@ if __name__=="__main__":
     test_inp = f_test.readlines()
     assert(tick_til_crash(test_inp) == (7, 3))
 
-    #f_test2 = open("test_input_2.txt", "r")
-    #test_inp2 = f_test2.readlines()
-    #assert(tick_til_one_left(test_inp2) == (6, 4))
+    # small test part 2
+    f_test2 = open("test_input_2.txt", "r")
+    test_inp2 = f_test2.readlines()
+    assert(tick_til_one_left(test_inp2) == (6, 4))
     
-    # problem
+    # problem part 1
     f = open("problem_input.txt", "r")
     inp = f.readlines()
-    #tick_til_crash(inp)
-    tick_til_one_left(inp)
-    #print(carts)
+    result = tick_til_crash(inp) # part 1
+    print("Result part one(first crash): " + str(result))
+
+    # problem part 2
+    f = open("problem_input.txt", "r")
+    inp_p2 = f.readlines()
+    result2 = tick_til_one_left(inp_p2) # part 1
+    print("Result part two(last crash): " + str(result2))
