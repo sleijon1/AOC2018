fbbebe0a2dbf3a1d75eeee9bb72481aa1e6e8124
sleijon1 <simonleijon59@gmail.com>
2020-10-06 20:25:44 +0200

    d14p1sp2ns

--- day14/day14.py
+++ day14/day14.py
@@ -1,23 +1,80 @@
 class Elf:
-    def __init__(self):
-        self.current_recipe = None
-    def set_recipe(self, recipe):
+    def __init__(self, i, recipe=None):
         self.current_recipe = recipe
+        self.index = i
     def __str__(self):
         return str("Elf(" + str(self.current_recipe)+ ")")
     def __repr__(self):
         return str(self)
 
-def setup(no_elfs=2):
-    score_board = [3, 7]
-    problem_input = 824501
+def produce_recipes(elfs, score_board, recipes=824501):
+    while len(score_board) < recipes+10:
+        recipe_sum = sum([elf.current_recipe for elf in elfs])
+        for new_rec in str(recipe_sum):
+            if len(score_board)+1 <= recipes+10:
+                score_board.append(int(new_rec))
+        for elf in elfs:
+            step_forward = 1+elf.current_recipe
+            if elf.index + step_forward > len(score_board)-1:
+                recipe_index = (elf.index + step_forward) % len(score_board) # loop around
+            else:
+                recipe_index = elf.index + step_forward
+            
+            elf.current_recipe = score_board[recipe_index]
+            elf.index = recipe_index
+    return score_board[len(score_board)-10:len(score_board)+1]
+
+def produce_recipes_two(elfs, score_board, score='824501'):
+    score_list = list(map(int, score))
+    print(score_list)
+    i = 0
+    j = 9
+    while j > 0:
+        if i == len(score_list):
+            return len(score_board) - len(score_list)
+        
+        recipe_sum = sum([elf.current_recipe for elf in elfs])
+        for new_rec in str(recipe_sum):
+            score_board.append(int(new_rec))
+            print(score_board)
+            if score_list[i] == int(new_rec):
+                i += 1
+            else:
+                i = 0
+            print(score_list[i])
+            print(i)                
+        for elf in elfs:
+            step_forward = 1+elf.current_recipe
+            if elf.index + step_forward > len(score_board)-1:
+                recipe_index = (elf.index + step_forward) % len(score_board) # loop around
+            else:
+                recipe_index = elf.index + step_forward
+            
+            elf.current_recipe = score_board[recipe_index]
+            elf.index = recipe_index
+        j -= 1 
+    return None
+
+
+def setup(score_board, no_elfs=2):
     elfs = []
-    for _ in range(no_elfs):
-        elfs.append(Elf())
-    print(elfs)
-    
+    for i in range(no_elfs):
+        new_elf = Elf(i, score_board[i])
+        elfs.append(new_elf)
+    return elfs
+
 if __name__ == "__main__":
-    score_board = [3, 7]
-    problem_input = 824501
-    print(setup())
-    
+    # part one
+    score_board=[3, 7]
+    elfs = setup(score_board)
+    last_ten = produce_recipes(elfs, score_board)
+    as_string = ''.join(map(str, last_ten))
+    print(score_board)
+    print(as_string)
+
+    # part two
+    score_board2=[3, 7]
+    elfs2 = setup(score_board)
+    left_digits = produce_recipes_two(elfs2, score_board2, score="01245")
+    print("digits to the left: " + str(left_digits))
+    #print("scoreboard2: " + str(score_board2))
