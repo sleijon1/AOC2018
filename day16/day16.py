ab3deb90e042f4ee1a417ba4fe80f6d16002d3bb
sleijon1 <simonleijon59@gmail.com>
2020-10-22 17:36:14 +0200

    day 16 part 1

--- day16/day16.py
+++ day16/day16.py
@@ -1,83 +1,101 @@
+def test_operations():
+    test_comp = Computer()
+    test_comp.set_regs([3,2,1,1])
+    test_comp.operations["mulr"]([2,1,2])
+    assert(test_comp.regs == [3,2,2,1])
+    test_comp.set_regs([3,2,1,1])
+    test_comp.addi([2,1,2])
+    assert(test_comp.regs == [3,2,2,1])
+    test_comp.set_regs([15,15,15,1])
+    test_comp.banr((1,2,3))
+    assert(test_comp.regs == [15,15,15,15])
+    test_comp.set_regs([14,1,15,1])
+    test_comp.bani((0, 1, 3))
+    assert(test_comp.regs == [14,1,15,0])
+    test_comp.set_regs([14,1,15,1])
+    test_comp.bori((0, 1, 3))
+    assert(test_comp.regs == [14,1,15,15])
+    test_comp.set_regs([1,2,15,1])
+    test_comp.borr((0,1,3))
+    assert(test_comp.regs == [1,2,15,3])
+    test_comp.set_regs([3,2,1,1])
+    test_comp.seti([2,1,2])
+    assert(test_comp.regs == [3,2,2,1])
+    test_comp.set_regs([3,1,1,1])
+    test_comp.gtir([2,1,0])
+    assert(test_comp.regs == [1,1,1,1])
+    test_comp.set_regs([3,1,3,1])
+    test_comp.gtri([2,2,0])
+    assert(test_comp.regs == [1,1,3,1])
+    test_comp.set_regs([0,1,1,1])
+    test_comp.eqri([2,1,0])
+    assert(test_comp.regs == [1,1,1,1])
+    print("passed all tests.")
+
 class Computer:
 
     def __init__(self, regs=[0, 0, 0, 0]):
-        self.operations = {}
+        self.operations = {
+            "eqir": self.eqir,
+            "eqri": self.eqri,
+            "eqrr": self.eqrr,
+            "gtir": self.gtir,
+            "gtri": self.gtri,
+            "gtrr": self.gtrr,
+            "setr": self.setr,
+            "seti": self.seti,
+            "borr": self.borr,
+            "bori": self.bori,
+            "bani": self.bani,
+            "banr": self.banr,
+            "mulr": self.mulr,
+            "muli": self.muli,
+            "addr": self.addr,
+            "addi": self.addi,
+
+        }
         self.regs = regs
 
     def set_regs(self, values=[0,0,0,0]):
         """ default - reset regs """
         self.regs = values
-
     def __str__(self):
         return str("16BIT, REGS: " + str(self.regs))
-
     def __repr__(self):
         return str(self)
 
-    def addr(self, op):
+    def eqir(self, op): #
+        self.regs[op[2]] = int(op[0] == self.regs[op[1]])
+    def eqri(self, op): #
+        self.regs[op[2]] = int(self.regs[op[0]] == op[1])
+    def eqrr(self, op): #
+        self.regs[op[2]] = int(self.regs[op[0]] == self.regs[op[1]])
+    def gtir(self, op): #
+        self.regs[op[2]] = int(op[0] > self.regs[op[1]])
+    def gtri(self, op): #
+        self.regs[op[2]] = int(self.regs[op[0]] > op[1])
+    def gtrr(self, op): #
+        self.regs[op[2]] = int(self.regs[op[0]] > self.regs[op[1]])
+    def setr(self, op): #
+        self.regs[op[2]] = self.regs[op[0]]
+    def seti(self, op): #
+        self.regs[op[2]] = op[0]
+    def addr(self, op): #
         self.regs[op[2]] = self.regs[op[1]] + self.regs[op[0]]
-    def addi(self, op):
+    def addi(self, op): #
         self.regs[op[2]] = op[1] + self.regs[op[0]]
-    def mulr(self, op):
+    def mulr(self, op): #
         self.regs[op[2]] = self.regs[op[1]] * self.regs[op[0]]
-    def muli(self, op):
+    def muli(self, op): #
         self.regs[op[2]] = op[1] * self.regs[op[0]]
-    def banr(self, op):
-        bits_one = bin(self.regs[op[1]])[2:]
-        bits_two = bin(self.regs[op[0]])[2:]
-        bits_one = ('0'*(4-len(bits_one)))+bits_one
-        bits_two = ('0'*(4-len(bits_two)))+bits_two
-        result = ""
-        for bit1, bit2 in zip(bits_one, bits_two):
-            if int(bit1) and int(bit2):
-                result += "1"
-            else:
-                result += "0"
-        result = "0b" + result
-        result = int(result, 2)
-        self.regs[op[2]] = result
-    def bani(self, op):
-        bits_one = bin(op[1])[2:] # value B
-        bits_two = bin(self.regs[op[0]])[2:] # register A
-        bits_one = ('0'*(4-len(bits_one)))+bits_one
-        bits_two = ('0'*(4-len(bits_two)))+bits_two
-        result = ""
-        for bit1, bit2 in zip(bits_one, bits_two):
-            if int(bit1) and int(bit2):
-                result += "1"
-            else:
-                result += "0"
-        result = "0b" + result
-        result = int(result, 2)
-        self.regs[op[2]] = result
-    def borr(self, op):
-        bits_one = bin(self.regs[op[1]])[2:]
-        bits_two = bin(self.regs[op[0]])[2:]
-        bits_one = ('0'*(4-len(bits_one)))+bits_one
-        bits_two = ('0'*(4-len(bits_two)))+bits_two
-        result = ""
-        for bit1, bit2 in zip(bits_one, bits_two):
-            if int(bit1) or int(bit2):
-                result += "1"
-            else:
-                result += "0"
-        result = "0b" + result
-        result = int(result, 2)
-        self.regs[op[2]] = result
-    def bori(self, op):
-        bits_one = bin(op[1])[2:] # value B
-        bits_two = bin(self.regs[op[0]])[2:] # register A
-        bits_one = ('0'*(4-len(bits_one)))+bits_one
-        bits_two = ('0'*(4-len(bits_two)))+bits_two
-        result = ""
-        for bit1, bit2 in zip(bits_one, bits_two):
-            if int(bit1) or int(bit2):
-                result += "1"
-            else:
-                result += "0"
-        result = "0b" + result
-        result = int(result, 2)
-        self.regs[op[2]] = result
+    def banr(self, op): #
+        self.regs[op[2]] = self.regs[op[1]] & self.regs[op[0]]
+    def bani(self, op): #
+        self.regs[op[2]] = op[1] & self.regs[op[0]]
+    def borr(self, op): #
+        self.regs[op[2]] = self.regs[op[1]] | self.regs[op[0]]
+    def bori(self, op): #
+        self.regs[op[2]] = op[1] | self.regs[op[0]]
 
 
 def read_file():
@@ -108,30 +126,39 @@ def read_file():
                         if char not in (',',' ', '[', ']')]
     return formatted
 
-def test_operations():
-    test_comp = Computer()
-    test_comp.set_regs([3,2,1,1])
-    test_comp.mulr([2,1,2])
-    assert(test_comp.regs == [3,2,2,1])
-    test_comp.set_regs([3,2,1,1])
-    test_comp.addi([2,1,2])
-    assert(test_comp.regs == [3,2,2,1])
-    test_comp.set_regs([15,15,15,1])
-    test_comp.banr((1,2,3))
-    assert(test_comp.regs == [15,15,15,15])
-    test_comp.set_regs([14,1,15,1])
-    test_comp.bani((0, 1, 3))
-    assert(test_comp.regs == [14,1,15,0])
-    test_comp.set_regs([14,1,15,1])
-    test_comp.bori((0, 1, 3))
-    assert(test_comp.regs == [14,1,15,15])
-    test_comp.set_regs([1,2,15,1])
-    test_comp.borr((0,1,3))
-    assert(test_comp.regs == [1,2,15,3])
-    print(test_comp)
+def test_opcodes(inp):
+    #inp = [[[3,2,1,1],[9,2,1,2],[3,2,2,1]]]
+    computer = Computer()
+    computer.set_regs()
+    op_names = computer.operations.keys()
+
+    three_or_more = 0
+    for sample in inp:
+        ops = []
+        possible_op = 0
+        before = sample[0]
+        instruction = sample[1][1:4] # skip opcode
+        after = sample[2]
+        for op in op_names:
+            computer.set_regs(list(before))
+            #print("actual before:" + str(before))
+            #print("before:" + str(computer.regs))
+            #print("op: " + op)
+            computer.operations[op](instruction)
+            if computer.regs == after:
+                #print("+1")
+                ops.append(op)
+                possible_op += 1
+        if possible_op == 2:
+            print(ops)
+            print(sample[1][0])
+        if possible_op >= 3:
+            three_or_more += 1
+    print(three_or_more)
 
 if __name__ == "__main__":
     inp = read_file()
     test_operations()
+    test_opcodes(inp)
     #print(inp)
 
