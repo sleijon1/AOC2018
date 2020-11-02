71bd817d0874b8918fc78dcddd896a5999a8d0bc
sleijon1 <simonleijon59@gmail.com>
2020-11-02 21:39:35 +0100

    part 1

--- day22/day22.py
+++ day22/day22.py
@@ -8,18 +8,21 @@ geologic_index = {
 
 erosion_levels = {
     (0, 0): depth%20183,
-    (target_x, target_y): 0,
+    (target_x, target_y): depth%20183,
 }
 
 for x in range(target_x+1):
     print(x)
     for y in range(target_y+1):
-        if y == 0:
+        if (x == 0 and y == 0) or (x == target_x and y == target_y):
+            continue
+        elif y == 0:
             geologic_index[(x, y)] = x*16807
         elif x == 0:
             geologic_index[(x, y)] = y*48271
         else:
-            geologic_index[(x, y)] = erosion_levels[(x-1, y)] * erosion_levels[(x, y-1)]
+            geologic_index[(x, y)] = erosion_levels[(x-1, y)] \
+                * erosion_levels[(x, y-1)]
         erosion_levels[(x, y)] = (geologic_index[(x, y)] + depth) % 20183
 
 print(sum([erosion % 3 for erosion in erosion_levels.values()]))
