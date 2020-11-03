29488852588655bf4b1acbd928ee580f412361e4
sleijon1 <simonleijon59@gmail.com>
2020-11-03 16:18:58 +0100

    fixed bug part 2 - done

--- day22/day22.py
+++ day22/day22.py
@@ -1,9 +1,9 @@
-from time import sleep
 from collections import deque
 from copy import deepcopy
+from time import sleep
 
-depth = 510
-target_x, target_y = (10,10)
+depth = 11109
+target_x, target_y = (9, 731)
 geologic_index = {
     (0, 0): 0,
     (target_x, target_y): 0,
@@ -30,7 +30,7 @@ def part_one():
     print("Solution part 1: " + str(sum([erosion % 3 for erosion in erosion_levels.values()])))
 
 def part_two():
-    for x in range(target_x+1):
+    for x in range(target_x+15):
         for y in range(target_y+1):
             if (x == 0 and y == 0) or (x == target_x and y == target_y):
                 continue
@@ -43,7 +43,7 @@ def part_two():
                     * erosion_levels[(x, y-1)]
             erosion_levels[(x, y)] = (geologic_index[(x, y)] + depth) % 20183
     type_dict = {}
-    for key in erosion_levels.keys():
+    for key in erosion_levels:
         mod = erosion_levels[key] % 3
         if mod == 2:
             type_dict[key] = '|'
@@ -52,86 +52,78 @@ def part_two():
         elif mod == 0:
             type_dict[key] = '.'
     queue = deque()
-    queue.append([[], 0, "torch", 0, (0, 0)])
+    queue.append([[], 0, "torch", (0, 0)])
     visited = list()
     time = []
+    cost_table = {(0, 0):
+                  (0, "torch")}
     while True:
         if not queue:
             break
         current = queue.pop()
         visited.append(current)
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        path, minutes, tool, swaps, pos = current
+        directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+        path, minutes, tool, pos = current
         new_path = list(path)
+
         new_path.append(pos)
-        #print("queue: " + str(queue))
-        #sleep(1)
         if pos == (target_x, target_y):
-            #if tool != "torch":
-                #minutes += 7
-            print("Found mans in: " + str(minutes) + " minutes.")
-            print("Path: " + str(path))
-            print("Current: " + str(current))
+            print("Solution part 2:\nFound Santa's friend in: " + str(minutes) + " minutes.")
             exit()
-        print("current node: " + str(current))
-        #print(visited)
         x, y = pos
-        #if (x, y) == (10, 9):
-        #    exit()
         queue_items = [(x+direction[0], y+direction[1]) for
                        direction in directions
-                       if (x+direction[0], y+direction[1]) in type_dict.keys()
-                       and (x+direction[0], y+direction[1]) not in new_path]
-        #print("queue_items: " + str(queue_items))
+                       if (x+direction[0], y+direction[1]) in type_dict.keys()]
         new_nodes = []
         for item in queue_items:
-            #print("current item: " + str(item))
-            #print("type dict item: " + str(type_dict[item]))
-            if type_dict[item] == '.':
+            if type_dict[item] == '.' and item == (target_x, target_y):
+                if tool != "torch":
+                    new_nodes.append([new_path, minutes+8, "torch",
+                                      item])
+                else:
+                    new_nodes.append([new_path, minutes+1, tool, item])
+            elif type_dict[item] == '.':
                 if tool not in ("torch", "climbing_gear"):
                     new_nodes.append([new_path, minutes+8, "torch",
-                                      swaps+1, item])
+                                       item])
                     new_nodes.append([new_path, minutes+8,
                                       "climbing_gear",
-                                      swaps+1, item])
+                                      item])
                 else:
-                    new_nodes.append([new_path, minutes+1, tool, swaps, item])
+                    new_nodes.append([new_path, minutes+1, tool, item])
             elif type_dict[item] == '|':
                 if tool not in ("torch", "neither"):
                     new_nodes.append([new_path, minutes+8, "torch",
-                                      swaps+1, item])
+                                       item])
                     new_nodes.append([new_path, minutes+8, "neither",
-                                      swaps+1, item])
+                                       item])
                 else:
-                    new_nodes.append([new_path, minutes+1, tool, swaps, item])
+                    new_nodes.append([new_path, minutes+1, tool, item])
             elif type_dict[item] == '=':
                 if tool not in ("climbing_gear", " neither"):
                     new_nodes.append([new_path, minutes+8, "neither",
-                                      swaps+1, item])
+                                       item])
                     new_nodes.append([new_path, minutes+8,
                                       "climbing_gear",
-                                      swaps+1, item])
+                                       item])
                 else:
-                    new_nodes.append([new_path, minutes+1, tool, swaps, item])
-        #print("new_nodes: " + str(new_nodes))
-        #print("len new nodes: " + str(len(new_nodes)))
-        #new_nodes = sorted(new_nodes, key=lambda x: x[1])
-        #new_nodes.reverse()
+                    new_nodes.append([new_path, minutes+1, tool, item])
+
         for new_node in new_nodes:
-            same_nodes = [visit for visit in visited
-                         if visit[-1] == new_node[-1]
-                         and visit[1] <= new_node[1]]
-            if same_nodes:
-               continue
+            try:
+                # (cost, tool)
+                key = (new_node[-1], new_node[2])
+                if cost_table[key] <= new_node[1]:
+                    continue
+            except KeyError:
+                cost_table[key] = new_node[1]
+
             queue.appendleft(new_node)
             visited.append(new_node)
-        #print(queue)
         new_deck = list(queue)
         new_deck = sorted(new_deck, key=lambda x: x[1])
         list.reverse(new_deck)
         queue = deque(new_deck)
-        #print(new_deck)
-
-
+    print(time)
 part_one()
 part_two()
